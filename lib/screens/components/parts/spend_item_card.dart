717b0e75c16e07ae6dddf10093faf3fd3010f25e
hidechy <hide.toyoda@gmail.com>
2024-03-26 18:34:45 +0900

    commit 20240326

--- lib/screens/components/parts/spend_item_card.dart
+++ lib/screens/components/parts/spend_item_card.dart
@@ -43,65 +43,70 @@ class SpendItemCard extends StatelessWidget {
       margin: const EdgeInsets.only(bottom: 5),
       decoration: BoxDecoration(border: Border.all(color: Colors.white.withOpacity(0.2))),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(spendItemName, style: TextStyle(color: Color(colorCode.toInt()))),
-          Row(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    alignment: Alignment.topRight,
-                    child: Text(
-                      (spendTimePlaceCountMap[spendItemName] != null)
-                          ? spendTimePlaceCountMap[spendItemName]!.length.toString()
-                          : 0.toString(),
-                      style: const TextStyle(color: Colors.grey),
+          SizedBox(
+            width: context.screenSize.width * 0.4,
+            child: Text(
+              spendItemName,
+              style: TextStyle(color: Color(colorCode.toInt())),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.topRight,
+                      child: Text(
+                        (spendTimePlaceCountMap[spendItemName] != null) ? spendTimePlaceCountMap[spendItemName]!.length.toString() : 0.toString(),
+                        style: const TextStyle(color: Colors.grey),
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 5),
-                  (spendTimePlaceCountMap[spendItemName] == null)
-                      ? Container()
-                      : (spendTimePlaceCountMap[spendItemName]!.isEmpty)
-                          ? const Icon(Icons.check_box_outline_blank, color: Colors.transparent)
-                          : GestureDetector(
-                              onTap: () {
-                                MoneyDialog(
-                                  context: context,
-                                  widget: SpendItemHistoryAlert(
-                                    date: DateTime.now(),
-                                    isar: isar,
-                                    item: spendItemName,
-                                    sum: 0,
-                                    from: 'spend_item_card',
-                                  ),
-                                );
-                              },
-                              child: Icon(Icons.list, color: Colors.white.withOpacity(0.2)),
-                            ),
-                ],
-              ),
-              const SizedBox(width: 20),
-              GestureDetector(
-                onTap: defaultTimeButtonPress,
-                child: Icon(
-                  Icons.access_time_outlined,
-                  color: (exDefaultTime[0].toInt() == 0)
-                      ? Colors.yellowAccent.withOpacity(0.3)
-                      : Colors.white.withOpacity(0.2),
+                    const SizedBox(width: 5),
+                    (spendTimePlaceCountMap[spendItemName] == null)
+                        ? Container()
+                        : (spendTimePlaceCountMap[spendItemName]!.isEmpty)
+                            ? const Icon(Icons.check_box_outline_blank, color: Colors.transparent)
+                            : GestureDetector(
+                                onTap: () {
+                                  MoneyDialog(
+                                    context: context,
+                                    widget: SpendItemHistoryAlert(
+                                      date: DateTime.now(),
+                                      isar: isar,
+                                      item: spendItemName,
+                                      sum: 0,
+                                      from: 'spend_item_card',
+                                    ),
+                                  );
+                                },
+                                child: Icon(Icons.list, color: Colors.white.withOpacity(0.2)),
+                              ),
+                  ],
                 ),
-              ),
-              const SizedBox(width: 10),
-              GestureDetector(
-                onTap: colorPickerButtonPress,
-                child: Icon(Icons.color_lens_outlined, color: Colors.white.withOpacity(0.2)),
-              ),
-              const SizedBox(width: 10),
-              GestureDetector(
-                onTap: deleteButtonPress,
-                child: Icon(Icons.delete, color: Colors.white.withOpacity(0.2)),
-              ),
-            ],
+                const SizedBox(width: 20),
+                GestureDetector(
+                  onTap: defaultTimeButtonPress,
+                  child: Icon(
+                    Icons.access_time_outlined,
+                    color: (exDefaultTime[0].toInt() == 0) ? Colors.yellowAccent.withOpacity(0.3) : Colors.white.withOpacity(0.2),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                GestureDetector(
+                  onTap: colorPickerButtonPress,
+                  child: Icon(Icons.color_lens_outlined, color: Colors.white.withOpacity(0.2)),
+                ),
+                const SizedBox(width: 10),
+                GestureDetector(
+                  onTap: deleteButtonPress,
+                  child: Icon(Icons.delete, color: Colors.white.withOpacity(0.2)),
+                ),
+              ],
+            ),
           ),
         ],
       ),
