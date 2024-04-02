81b04030858f544c91f7b2d035375a75258b9982
toyotarou <toyodahideyuki4819@gmail.com>
2024-04-02 19:58:06 +0900

    commit 20240402

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
+/////////
+
 import 'collections/bank_name.dart';
 import 'collections/bank_price.dart';
 import 'collections/emoney_name.dart';
