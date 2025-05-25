3a5dc7d6c1a450076aaa5ad7b9d68e902e913538
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-05-25 19:29:45 +0900

    commit 20250525

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,22 +1,64 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar/isar.dart';
 
+import '../collections/config.dart';
+import '../collections/login_account.dart';
+import '../controllers/controllers_mixin.dart';
 import '../extensions/extensions.dart';
+import '../repository/configs_repository.dart';
+import '../repository/login_accounts_repository.dart';
+import '../utilities/functions.dart';
 import 'components/parts/custom_shape_clipper.dart';
+import 'components/parts/error_dialog.dart';
 import 'home_screen.dart';
 import 'signup_screen.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
-  const LoginScreen({super.key, required this.isar});
+  const LoginScreen({super.key, required this.isar, this.mailAddress, this.password});
 
   final Isar isar;
+
+  final String? mailAddress;
+  final String? password;
 
   @override
   ConsumerState<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends ConsumerState<LoginScreen> {
+class _LoginScreenState extends ConsumerState<LoginScreen> with ControllersMixin<LoginScreen> {
+  TextEditingController mailAddressEditingController = TextEditingController();
+
+  TextEditingController passwordEditingController = TextEditingController();
+
+  List<FocusNode> focusNodeList = <FocusNode>[];
+
+  List<LoginAccount>? loginAccountList = <LoginAccount>[];
+
+  Map<String, String> loginAccountMap = <String, String>{};
+
+  Map<String, String> configMap = <String, String>{};
+
+  ///
+  @override
+  void initState() {
+    super.initState();
+
+    // ignore: always_specify_types
+    focusNodeList = List.generate(100, (int index) => FocusNode());
+
+    _makeLoginAccountList();
+
+    _makeConfigMap();
+
+    if (widget.mailAddress != null && widget.password != null) {
+      mailAddressEditingController.text = widget.mailAddress!;
+      passwordEditingController.text = widget.password!;
+    }
+  }
+
   ///
   @override
   Widget build(BuildContext context) {
@@ -42,7 +84,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
             child: Column(
               children: <Widget>[
                 SizedBox(width: context.screenSize.width),
-                const SizedBox(height: 60),
+                const SizedBox(height: 30),
                 Container(
                   width: context.screenSize.width / 2.5,
                   height: 140,
@@ -55,16 +97,8 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                   decoration: const BoxDecoration(
                       image: DecorationImage(image: AssetImage('assets/images/moneynote_title.png'))),
                 ),
-                const SizedBox(height: 60),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        // ignore: inference_failure_on_instance_creation, always_specify_types
-                        MaterialPageRoute(builder: (BuildContext context) => HomeScreen(isar: widget.isar)));
-                  },
-                  child: const Text('LOGIN'),
-                ),
+                const SizedBox(height: 20),
+                _displayInputParts(),
               ],
             ),
           ),
@@ -91,5 +125,161 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
         ],
       ),
     );
+  }
+
+  ///
+  Widget _displayInputParts() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[
+          BoxShadow(blurRadius: 24, spreadRadius: 16, color: Colors.black.withOpacity(0.2)),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+          child: Container(
+            width: context.screenSize.width,
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.white.withOpacity(0.2), width: 1.5),
+            ),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 20),
+                TextField(
+                  controller: mailAddressEditingController,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                    hintText: 'メールアドレス(30文字以内)',
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white54)),
+                  ),
+                  style: const TextStyle(fontSize: 13, color: Colors.white),
+                  onTapOutside: (PointerDownEvent event) => FocusManager.instance.primaryFocus?.unfocus(),
+                  focusNode: focusNodeList[0],
+                  onTap: () => context.showKeyboard(focusNodeList[0]),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: passwordEditingController,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                    hintText: 'パスワード(30文字以内)',
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white54)),
+                  ),
+                  style: const TextStyle(fontSize: 13, color: Colors.white),
+                  onTapOutside: (PointerDownEvent event) => FocusManager.instance.primaryFocus?.unfocus(),
+                  focusNode: focusNodeList[1],
+                  onTap: () => context.showKeyboard(focusNodeList[1]),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    _login();
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.pinkAccent.withOpacity(0.2)),
+                  child: const Text('LOGIN'),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ///
+  Future<void> _makeLoginAccountList() async {
+    await LoginAccountsRepository().getLoginAccountList(isar: widget.isar).then(
+      (List<LoginAccount>? value) {
+        if (mounted) {
+          setState(
+            () {
+              loginAccountList = value;
+
+              if (value!.isNotEmpty) {
+                for (final LoginAccount element in value) {
+                  loginAccountMap[element.mailAddress] = element.password;
+                }
+              }
+            },
+          );
+        }
+      },
+    );
+  }
+
+  ///
+  Future<void> _makeConfigMap() async {
+    await ConfigsRepository().getConfigList(isar: widget.isar).then(
+      (List<Config>? value) {
+        if (mounted) {
+          setState(
+            () {
+              if (value!.isNotEmpty) {
+                for (final Config element in value) {
+                  configMap[element.configKey] = element.configValue;
+                }
+              }
+            },
+          );
+        }
+      },
+    );
+  }
+
+  ///
+  void _login() {
+    bool errFlg = false;
+
+    if (mailAddressEditingController.text.trim() == '' || passwordEditingController.text.trim() == '') {
+      errFlg = true;
+    }
+
+    if (!errFlg) {
+      for (final List<Object> element in <List<Object>>[
+        <Object>[mailAddressEditingController.text.trim(), 30],
+        <Object>[passwordEditingController.text.trim(), 30],
+      ]) {
+        if (!checkInputValueLengthCheck(value: element[0].toString(), length: element[1] as int)) {
+          errFlg = true;
+        }
+      }
+    }
+
+    if (loginAccountMap[mailAddressEditingController.text.trim()] != null) {
+      if (passwordEditingController.text.trim() == loginAccountMap[mailAddressEditingController.text.trim()]) {
+        Navigator.pushReplacement(
+            context,
+            // ignore: inference_failure_on_instance_creation, always_specify_types
+            MaterialPageRoute(builder: (BuildContext context) => HomeScreen(isar: widget.isar)));
+      } else {
+        errFlg = true;
+      }
+    } else {
+      errFlg = true;
+    }
+
+    if (errFlg) {
+      // ignore: always_specify_types, use_build_context_synchronously
+      Future.delayed(
+          Duration.zero,
+          // ignore: use_build_context_synchronously
+          () => error_dialog(context: context, title: 'ログインできません。', content: '値を正しく入力してください。'));
+
+      return;
+    }
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,9 +1,16 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar/isar.dart';
 
+import '../collections/login_account.dart';
+import '../controllers/controllers_mixin.dart';
 import '../extensions/extensions.dart';
+import '../repository/login_accounts_repository.dart';
+import '../utilities/functions.dart';
 import 'components/parts/custom_shape_clipper.dart';
+import 'components/parts/error_dialog.dart';
 import 'login_screen.dart';
 
 class SignupScreen extends ConsumerStatefulWidget {
@@ -15,7 +22,25 @@ class SignupScreen extends ConsumerStatefulWidget {
   ConsumerState<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _SignupScreenState extends ConsumerState<SignupScreen> {
+class _SignupScreenState extends ConsumerState<SignupScreen> with ControllersMixin<SignupScreen> {
+  TextEditingController mailAddressEditingController = TextEditingController();
+
+  TextEditingController passwordEditingController = TextEditingController();
+
+  List<FocusNode> focusNodeList = <FocusNode>[];
+
+  ///
+  @override
+  void initState() {
+    super.initState();
+
+    // ignore: always_specify_types
+    focusNodeList = List.generate(100, (int index) => FocusNode());
+
+    mailAddressEditingController.clear();
+    passwordEditingController.clear();
+  }
+
   ///
   @override
   Widget build(BuildContext context) {
@@ -41,7 +66,7 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
             child: Column(
               children: <Widget>[
                 SizedBox(width: context.screenSize.width),
-                const SizedBox(height: 60),
+                const SizedBox(height: 30),
                 Container(
                   width: context.screenSize.width / 2.5,
                   height: 140,
@@ -54,16 +79,8 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
                   decoration: const BoxDecoration(
                       image: DecorationImage(image: AssetImage('assets/images/moneynote_title.png'))),
                 ),
-                const SizedBox(height: 60),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        // ignore: inference_failure_on_instance_creation, always_specify_types
-                        MaterialPageRoute(builder: (BuildContext context) => LoginScreen(isar: widget.isar)));
-                  },
-                  child: const Text('SIGN UP'),
-                ),
+                const SizedBox(height: 20),
+                _displayInputParts(),
               ],
             ),
           ),
@@ -89,6 +106,129 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  ///
+  Widget _displayInputParts() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[
+          BoxShadow(blurRadius: 24, spreadRadius: 16, color: Colors.black.withOpacity(0.2)),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+          child: Container(
+            width: context.screenSize.width,
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.white.withOpacity(0.2), width: 1.5),
+            ),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 20),
+                TextField(
+                  controller: mailAddressEditingController,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                    hintText: 'メールアドレス(30文字以内)',
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white54)),
+                  ),
+                  style: const TextStyle(fontSize: 13, color: Colors.white),
+                  onTapOutside: (PointerDownEvent event) => FocusManager.instance.primaryFocus?.unfocus(),
+                  focusNode: focusNodeList[0],
+                  onTap: () => context.showKeyboard(focusNodeList[0]),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: passwordEditingController,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                    hintText: 'パスワード(30文字以内)',
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white54)),
+                  ),
+                  style: const TextStyle(fontSize: 13, color: Colors.white),
+                  onTapOutside: (PointerDownEvent event) => FocusManager.instance.primaryFocus?.unfocus(),
+                  focusNode: focusNodeList[1],
+                  onTap: () => context.showKeyboard(focusNodeList[1]),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    _inputLoginAccount();
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.pinkAccent.withOpacity(0.2)),
+                  child: const Text('SIGN UP'),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ///
+  Future<void> _inputLoginAccount() async {
+    bool errFlg = false;
+
+    if (mailAddressEditingController.text.trim() == '' || passwordEditingController.text.trim() == '') {
+      errFlg = true;
+    }
+
+    if (!errFlg) {
+      for (final List<Object> element in <List<Object>>[
+        <Object>[mailAddressEditingController.text.trim(), 30],
+        <Object>[passwordEditingController.text.trim(), 30],
+      ]) {
+        if (!checkInputValueLengthCheck(value: element[0].toString(), length: element[1] as int)) {
+          errFlg = true;
+        }
+      }
+    }
+
+    if (errFlg) {
+      // ignore: always_specify_types, use_build_context_synchronously
+      Future.delayed(Duration.zero, () => error_dialog(context: context, title: '登録できません。', content: '値を正しく入力してください。'));
+
+      return;
+    }
+
+    final LoginAccount loginAccount = LoginAccount()
+      ..mailAddress = mailAddressEditingController.text.trim()
+      ..password = passwordEditingController.text.trim();
+
+    await LoginAccountsRepository().inputLoginAccount(isar: widget.isar, loginAccount: loginAccount).then(
+      // ignore: always_specify_types
+      (value) {
+        Navigator.pushReplacement(
+          // ignore: use_build_context_synchronously
+          context,
+
+          // ignore: inference_failure_on_instance_creation, always_specify_types
+          MaterialPageRoute(
+            builder: (BuildContext context) => LoginScreen(
+              isar: widget.isar,
+              mailAddress: mailAddressEditingController.text.trim(),
+              password: passwordEditingController.text.trim(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
